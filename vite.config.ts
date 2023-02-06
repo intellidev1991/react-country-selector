e6f0e4bd792de6490105eafd9b598dbdd2a67311
Hamed Taheri <hamed_taheri@yahoo.com>
2023-02-06 02:36:47 -0600

    fix: npm publisher

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { UserConfig, defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
@@ -8,7 +9,19 @@ import { peerDependencies, dependencies } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig((_configEnv) => ({
-  plugins: [dts(), react(), tsConfigPaths()],
+  plugins: [
+    dts(),
+    react(),
+    tsConfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'package.json',
+          dest: './',
+        },
+      ],
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, './src', 'index.ts'),
